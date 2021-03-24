8890be6ee952c34be12c97575918bb95948e5812
Patrick Cook <ptcook74@gmail.com>
2021-03-23 20:09:11 -0400

    mapping

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import PropsExample from "./components/PropsExample";
+import PropsMappingExample from "./components/PropsMapping";
 
 function App() {
-  return <div></div>;
+  const visitedPlaces = ["Puerto Rico", "Bahamas", "Honduras", "Belize", "Cayman Islands", "Dominican Republic"];
+  return (
+    <div>
+      <PropsExample name="Tom" business="MySpace" />
+      <PropsMappingExample visited={visitedPlaces} />
+    </div>
+  );
 }
 
 export default App;
